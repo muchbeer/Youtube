76852f37d66e59af5e886e13f07dd479b4b51b57
muchbeer <gman2them@gmail.com>
2015-06-24 04:25:07 +0300

    Initial commit

---
+++ app/src/main/java/muchbeer/king/youtube/Config.java
@@ -0,0 +1,12 @@
+package muchbeer.king.youtube;
+
+public class Config {
+	// Google Console APIs developer key
+	// Replace this key with your's
+	public static final String DEVELOPER_KEY = "AIzaSyAiMfgEbhD25wNx1-ds-MM90TBi8yjtc8I";
+	
+	// YouTube video id
+
+   // uj0S2rgmu-M    _oEA18Y8gM0
+	public static final String YOUTUBE_VIDEO_CODE = "uj0S2rgmu-M";
+}
